63a65dc3c91d74d6d9693c48c55d187f7aaccc5f
baranacikgoz <baran-acikgoz@outlook.com>
2023-01-12 00:44:11 +0300

    applied ReSharper suggestions

--- Fibonacci/Fibonacci/Program.cs
+++ Fibonacci/Fibonacci/Program.cs
@@ -1,5 +1,7 @@
-﻿Solution solution = new Solution();
+﻿Solution solution = new();
+
 Console.WriteLine(solution.Fib(5));
+
 public class Solution
 {
     public int Fib(int n)
@@ -9,17 +11,15 @@ public class Solution
             return 0;
         }
 
-        int[] table = new int[n + 1];
+        var table = new int[n + 1];
 
         // Seed the trivial answer.
         table[1] = 1;
 
         // Iterate and fill further positions based on current values.
-        for (int i = 0; i < n - 1; i++)
+        for (var i = 0; i < n - 1; i++)
         {
-
             table[i + 2] = table[i] + table[i + 1];
-
         }
         return table[n];
     }
