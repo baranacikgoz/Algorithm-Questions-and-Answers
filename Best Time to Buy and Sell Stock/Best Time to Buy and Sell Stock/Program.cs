11078256c3ccedf05fb81c17573634d53b605b8f
baranacikgoz <baran-acikgoz@outlook.com>
2023-01-11 23:55:33 +0300

    added 'Best Time to Buy and Sell Stock'

---
+++ Best Time to Buy and Sell Stock/Best Time to Buy and Sell Stock/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// [7,1,5,3,6,4]
+public class Solution
+{
+    public int MaxProfit(int[] prices)
+    {
+        int minPrice = int.MaxValue;
+
+        int maxProfit = 0;
+
+        for (int i = 0; i < prices.Length; i++)
+        {
+            minPrice = Math.Min(minPrice, prices[i]);
+
+            maxProfit = Math.Max(maxProfit, prices[i] - minPrice);
+        }
+
+        return maxProfit;
+    }
+}
---
+++ Best Time to Buy and Sell Stock/README.md
@@ -0,0 +1,3 @@
+# Best time to Buy and Sell Stock
+
+[leetcode-question-link](https://leetcode.com/problems/best-time-to-buy-and-sell-stock/)
