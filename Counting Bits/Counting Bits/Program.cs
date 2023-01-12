a057f5d3661ee21ede428f79baf8b9fad06bdb08
baranacikgoz <baran-acikgoz@outlook.com>
2023-01-12 13:36:39 +0300

    added 'Counting Bits' question

---
+++ Counting Bits/Counting Bits/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+Solution solution = new();
+var result = solution.CountBits(5);
+
+Console.WriteLine("[{0}]", string.Join(", ", result));
+
+public class Solution
+{
+    public int[] CountBits(int n)
+    {
+        var table = new int[n + 1];
+
+        int lastPowerOfTwo = 1; // 1,2,4,8,16,32,....
+
+        for (int i = 1; i < n + 1; i++)
+        {
+            if (i == lastPowerOfTwo * 2)
+            {
+                lastPowerOfTwo = i;
+            }
+
+            table[i] = table[i - lastPowerOfTwo] + 1;
+        }
+
+        return table;
+    }
+}
---
+++ Counting Bits/README.md
@@ -0,0 +1,14 @@
+# Counting Bits
+
+### [Problem Link](https://leetcode.com/problems/counting-bits/)
+
+### Way of Thinking
+![img1](way-of-thinking1.png)  
+
+***So;***  
+
+![img1](way-of-thinking2.png)
+
+### Solution Results
+
+![results](results.png)
