a8fc5b98d797379a0ffe39526794b9e64f284c8c
baranacikgoz <baran-acikgoz@outlook.com>
2023-01-31 00:49:00 +0300

    added Min Cost Climbing Stairs problem

---
+++ Min Cost Climbing Stairs/Min Cost Climbing Stairs/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] cost = new int[] { 10, 15, 20 };
+Solution solution = new Solution();
+int minCost = solution.MinCostClimbingStairs(cost);
+Console.WriteLine(minCost);
+
+public class Solution
+{
+    public int MinCostClimbingStairs(int[] cost)
+    {
+        int firstStep = cost[0];
+        int secondStep = cost[1];
+
+        for (int i = 2; i < cost.Length; i++)
+        {
+            int current = cost[i] + Math.Min(firstStep, secondStep);
+
+            firstStep = secondStep;
+            secondStep = current;
+        }
+
+        return Math.Min(firstStep, secondStep);
+    }
+}
---
+++ Min Cost Climbing Stairs/README.md
@@ -0,0 +1,7 @@
+# Min Cost Climbing Stairs
+
+### [Question Link](https://leetcode.com/problems/min-cost-climbing-stairs)
+
+### Solution Results
+
+![results](results.png)
