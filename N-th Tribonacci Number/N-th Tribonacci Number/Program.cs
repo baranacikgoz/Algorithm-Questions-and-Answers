9d7e414539e73877b14047a82cdb58554d57a109
baranacikgoz <baran-acikgoz@outlook.com>
2023-01-31 13:17:21 +0300

    added N-th Tribonacci Number problem

---
+++ N-th Tribonacci Number/N-th Tribonacci Number/Program.cs
@@ -0,0 +1,28 @@
+﻿Solution solution = new();
+Console.WriteLine(solution.Tribonacci(1));
+
+public class Solution
+{
+    public int Tribonacci(int n)
+    {
+        try
+        {
+            int[] dp = new int[n + 1];
+
+            dp[0] = 0;
+            dp[1] = 1;
+            dp[2] = 1;
+
+            for (int i = 0; i <= n - 3; i++)
+            {
+                dp[i + 3] = dp[i + 2] + dp[i + 1] + dp[i];
+            }
+
+            return dp[n];
+        }
+        catch (IndexOutOfRangeException)
+        {
+            return n == 0 ? 0 : 1;
+        }
+    }
+}
---
+++ N-th Tribonacci Number/README.md
@@ -0,0 +1,6 @@
+# N-th Tribonacci Number
+
+## [Question Link](https://leetcode.com/problems/n-th-tribonacci-number)
+
+### Solution Results
+![results](results.png)
